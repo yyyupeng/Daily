6429ced98e4bd4891990fd2a1ef807105a249787
liyupeng <liyupeng@lyp.linux>
2020-04-01 16:39:08 +0800

    .

--- 数据结构/AVL/AVLTree.h
+++ 数据结构/AVL/AVLTree.h
@@ -5,11 +5,7 @@
 #include <algorithm>
 using namespace std;
 
-
-//求树的高度
-//int GetHeight(AVLNode<T> *root);
-
-template <class T>
+template <typename T>
 class AVLNode
 {
 public:
@@ -18,10 +14,10 @@ public:
     AVLNode<T> *right;
     int height;
 
-    AVLNode(T value, AVLNode *l, AVLNode *r) : key(value), left(l), right(r), height(0) {}
+    AVLNode(T value, AVLNode<T> *l, AVLNode<T> *r) : key(value), left(l), right(r), height(0) {}
 };
 
-template <class T>
+template <typename T>
 class AVLTree
 {
 public:
@@ -31,23 +27,27 @@ public:
     //插入节点
     void Insert(AVLNode<T> *&root, T );
     //删除节点
-    bool Delete(AVLNode<T>* &root, T );
-    //前序遍历
+    bool Delete(AVLNode<T> *&root, T );
+    //中序遍历
     void InOrderTra(AVLNode<T> *root) const;
 
 private:
     //求树的高度
-    //int GetHeight(AVLNode<T> *root);
+    int GetHeight(AVLNode<T> *root);
+    //左旋
     AVLNode<T> *LL(AVLNode<T> *root);
+    //右旋
     AVLNode<T> *RR(AVLNode<T> *root);
+    //先左旋再右旋
     AVLNode<T> *LR(AVLNode<T> *root);
+    //先右旋再左旋
     AVLNode<T> *RL(AVLNode<T> *root);
     //销毁AVLTree
     void destory(AVLNode<T> *&root);
 };
 
 template <typename T>
-int GetHeight(AVLNode<T> *root)
+int AVLTree<T>::GetHeight(AVLNode<T> *root)
 {
     if(root != nullptr)
         return root->height;
@@ -68,7 +68,7 @@ AVLNode<T> * AVLTree<T>::LL(AVLNode<T> *root)
     root->right = q->left;
     q->left = root;
     root->height = max(GetHeight(root->left), GetHeight(root->right)) + 1;
-    q->height = max(GetHeight(q->left), GetHeight(q->right)) + 1;
+    q->height = max(root->height, GetHeight(q->right)) + 1;
     return q;
 }
 
@@ -79,21 +79,21 @@ AVLNode<T>* AVLTree<T>::RR(AVLNode<T> *root)
     root->left = q->right;
     q->right = root;
     root->height = max(GetHeight(root->left), GetHeight(root->right)) + 1;
-    q->height = max(GetHeight(q->left), GetHeight(q->right)) + 1;
+    q->height = max(GetHeight(q->left), root->height) + 1;
     return q;
 }
 
 template <typename T>
 AVLNode<T>* AVLTree<T>::LR(AVLNode<T> *root)
 {
-    LL(root->left);
+    root->left = LL(root->left);
     return RR(root);
 }
 
 template <typename T>
 AVLNode<T>* AVLTree<T>::RL(AVLNode<T> *root)
 {
-    RR(root->left);
+    root->right = RR(root->right);
     return LL(root);
 }
 
@@ -102,26 +102,30 @@ void AVLTree<T>::Insert(AVLNode<T> *&root, T key)
 {
     if(root == nullptr)
         root = new AVLNode<T>(key, nullptr, nullptr);
-    else if(key < root->key)
+    else if(key < root->key)    //插入到左子树中
     {
         Insert(root->left, key);
         //判断平衡情况
-        if(GetHeight(root->left) - GetHeight(root->right) == 2)
+        if(GetHeight(root->left) - GetHeight(root->right) >= 2)
         {
+            //左子树的左子树
             if(key < root->left->key)
                 root = RR(root);
+            //左子树的右子树
             else
                 root = LR(root);
         }
     }
-    else if(key > root->key)
+    else if(key > root->key)    //插入到右子树中
     {
         Insert(root->right, key);
         //判断平衡情况
-        if(GetHeight(root->right) - GetHeight(root->left) == 2)
+        if(GetHeight(root->right) - GetHeight(root->left) >= 2)
         {
+            //右子树的右子树
             if(key > root->right->key)
                 root = LL(root);
+            //右子树的左子树
             else
                 root = RL(root);
         }
@@ -129,7 +133,16 @@ void AVLTree<T>::Insert(AVLNode<T> *&root, T key)
     else
         cout << "添加失败！" << endl;
 
+    //更新高度
+    /* cout << key << " " << root->height << endl; */
     root->height = max(GetHeight(root->left), GetHeight(root->right)) + 1;
+    /* cout << key << " " << root->height << endl; */
+}
+
+template <typename T>
+bool AVLTree<T>::Delete(AVLNode<T> *&root, T key)
+{
+
 }
 
 template <typename T>
