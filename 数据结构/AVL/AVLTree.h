01dd9d3e9e148d2923551e546c3b7955822dfa7e
yupeng18 <1366116954@qq.com>
2020-04-02 00:23:05 +0800

    up

--- 数据结构/AVL/AVLTree.h
+++ 数据结构/AVL/AVLTree.h
@@ -27,9 +27,13 @@ public:
     //插入节点
     void Insert(AVLNode<T> *&root, T );
     //删除节点
-    bool Delete(AVLNode<T> *&root, T );
+    AVLNode<T> *Delete(AVLNode<T> *&root, T );
     //中序遍历
     void InOrderTra(AVLNode<T> *root) const;
+    //最小值节点
+    AVLNode<T> *FindMin(AVLNode<T> *root) const;
+    //最大值节点
+    AVLNode<T> *FindMax(AVLNode<T> *root) const;
 
 private:
     //求树的高度
@@ -55,6 +59,26 @@ int AVLTree<T>::GetHeight(AVLNode<T> *root)
 }
 
 template <typename T>
+AVLNode<T> *AVLTree<T>::FindMin(AVLNode<T> *root) const
+{
+    if(root == nullptr)
+        return nullptr;
+    if(root->left == nullptr)
+        return root;
+    return FindMin(root->left);
+}
+
+template <typename T>
+AVLNode<T> *AVLTree<T>::FindMax(AVLNode<T> *root) const
+{
+    if(root == nullptr)
+        return nullptr;
+    if(root->right == nullptr)
+        return root;
+    return FindMax(root->right);
+}
+
+template <typename T>
 AVLTree<T>::~AVLTree()
 {
     cout << "销毁AVL树..." << endl;
@@ -62,7 +86,7 @@ AVLTree<T>::~AVLTree()
 }
 
 template <typename T>
-AVLNode<T> * AVLTree<T>::LL(AVLNode<T> *root)
+AVLNode<T> *AVLTree<T>::LL(AVLNode<T> *root)
 {
     AVLNode<T> *q = root->right;
     root->right = q->left;
@@ -73,7 +97,7 @@ AVLNode<T> * AVLTree<T>::LL(AVLNode<T> *root)
 }
 
 template <typename T>
-AVLNode<T>* AVLTree<T>::RR(AVLNode<T> *root)
+AVLNode<T> *AVLTree<T>::RR(AVLNode<T> *root)
 {
     AVLNode<T> *q = root->left;
     root->left = q->right;
@@ -84,14 +108,14 @@ AVLNode<T>* AVLTree<T>::RR(AVLNode<T> *root)
 }
 
 template <typename T>
-AVLNode<T>* AVLTree<T>::LR(AVLNode<T> *root)
+AVLNode<T> *AVLTree<T>::LR(AVLNode<T> *root)
 {
     root->left = LL(root->left);
     return RR(root);
 }
 
 template <typename T>
-AVLNode<T>* AVLTree<T>::RL(AVLNode<T> *root)
+AVLNode<T> *AVLTree<T>::RL(AVLNode<T> *root)
 {
     root->right = RR(root->right);
     return LL(root);
@@ -106,7 +130,7 @@ void AVLTree<T>::Insert(AVLNode<T> *&root, T key)
     {
         Insert(root->left, key);
         //判断平衡情况
-        if(GetHeight(root->left) - GetHeight(root->right) >= 2)
+        if(GetHeight(root->left) - GetHeight(root->right) == 2)
         {
             //左子树的左子树
             if(key < root->left->key)
@@ -120,7 +144,7 @@ void AVLTree<T>::Insert(AVLNode<T> *&root, T key)
     {
         Insert(root->right, key);
         //判断平衡情况
-        if(GetHeight(root->right) - GetHeight(root->left) >= 2)
+        if(GetHeight(root->right) - GetHeight(root->left) == 2)
         {
             //右子树的右子树
             if(key > root->right->key)
@@ -134,15 +158,70 @@ void AVLTree<T>::Insert(AVLNode<T> *&root, T key)
         cout << "添加失败！" << endl;
 
     //更新高度
-    /* cout << key << " " << root->height << endl; */
     root->height = max(GetHeight(root->left), GetHeight(root->right)) + 1;
-    /* cout << key << " " << root->height << endl; */
 }
 
 template <typename T>
-bool AVLTree<T>::Delete(AVLNode<T> *&root, T key)
+AVLNode<T> *AVLTree<T>::Delete(AVLNode<T> *&root, T key)
 {
+    if(root == nullptr)
+        return nullptr;
 
+    if(key < root->key)         //删除的节点在左子树上
+    {
+        root->left = Delete(root->left, key);    
+        //判断是否还满足平衡条件
+        //删除左子树的节点相当于在右子树插入一个节点
+        if(GetHeight(root->right) - GetHeight(root->left) == 2)
+        {
+            if(GetHeight(root->right->left) > GetHeight(root->right->right))
+                root = RL(root);
+            else 
+                root = RR(root);
+        }
+    }
+    else if(key > root->key)    //删除的节点在右子树上
+    {
+        root->right = Delete(root->right, key);
+        //判断是否还满足平衡条件
+        //删除右子树的节点相当于在左子树插入一个节点
+        if(GetHeight(root->left) - GetHeight(root->right) == 2)
+        {
+            if(GetHeight(root->left->left) > GetHeight(root->left->right))
+                root = LL(root);
+            else
+                root = LR(root);
+        }
+    }
+    else        //找到了要删除的节点
+    {
+        //左右子树都非空
+        //在左右子树中较高的子树中进行删除
+        if(root->left != nullptr && root->right != nullptr)
+        {
+            //左子树高,将其值最大的节点赋值给删除节点,然后删除
+            if(GetHeight(root->left) >= GetHeight(root->right))
+            {
+                AVLNode<T> *MaxNode = FindMax(root->left);
+                root->key = MaxNode->key;
+                root->left = Delete(root->left, key);
+            }
+            //右子树高,将其值最小的节点赋值给删除节点,然后删除
+            else
+            {
+                AVLNode<T> *MinNode = FindMin(root->right);
+                root->key = MinNode->key;
+                root->right = Delete(root->right, key);
+            }
+        }
+        else
+        {
+            AVLNode<T> *temp = root;
+            root = (root->left != NULL) ? root->left : root->right;
+            delete temp;
+        }
+    }
+    return root;
 }
 
 template <typename T>
--- 数据结构/AVL/main.cpp
+++ 数据结构/AVL/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     AVLTree<int> tree;
     int value;
+    int temp;
     cout << "请输入整数建立二叉树(-1结束)：" << endl;
     while (cin >> value)
     {
@@ -16,6 +17,14 @@ int main()
     tree.InOrderTra(tree.root);
     cout << endl;
 
+    cout << "删除那个节点? ";
+    cin >> temp;
+    AVLNode<int> *node = tree.Delete(tree.root, temp);
+
+    cout << "删除后中序遍历：";
+    tree.InOrderTra(tree.root);
+    cout << endl;
+    
     return 0;
 }
 
